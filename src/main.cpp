cc32e9dcef36a06a3346a4c4de60730d2799eda9
Radko Apostolov <rapostolov94@gmail.com>
2026-04-11 17:40:26 +0300

    first successful build

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include "DHTesp.h"
+
+// Task 1: read temperature/humidity and control an LED using a simple threshold.
+const int DHT_PIN = 4;
+const int LED_PIN = 18;
+const float TEMP_THRESHOLD_C = 28.0;
+const unsigned long READ_INTERVAL_MS = 2000;
+
+DHTesp dhtSensor;
+
+void printStartupMessage() {
+  Serial.println();
+  Serial.println("Theme 5 - Task 1");
+  Serial.println("ESP32 + DHT22 + LED threshold demo");
+  Serial.print("Temperature threshold: ");
+  Serial.print(TEMP_THRESHOLD_C, 1);
+  Serial.println(" C");
+  Serial.print("Minimum DHT sampling period: ");
+  Serial.print(dhtSensor.getMinimumSamplingPeriod());
+  Serial.println(" ms");
+  Serial.println();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+  printStartupMessage();
+}
+
+void loop() {
+  TempAndHumidity data = dhtSensor.getTempAndHumidity();
+
+  if (dhtSensor.getStatus() != 0) {
+    digitalWrite(LED_PIN, LOW);
+    Serial.print("Sensor read error: ");
+    Serial.println(dhtSensor.getStatusString());
+    Serial.println("LED state: OFF");
+    Serial.println();
+    delay(READ_INTERVAL_MS);
+    return;
+  }
+
+  bool thresholdExceeded = data.temperature > TEMP_THRESHOLD_C;
+  digitalWrite(LED_PIN, thresholdExceeded ? HIGH : LOW);
+
+  Serial.print("Temperature: ");
+  Serial.print(data.temperature, 1);
+  Serial.println(" C");
+
+  Serial.print("Humidity: ");
+  Serial.print(data.humidity, 1);
+  Serial.println(" %");
+
+  Serial.print("Threshold status: ");
+  Serial.println(thresholdExceeded ? "ABOVE LIMIT" : "NORMAL");
+
+  Serial.print("LED state: ");
+  Serial.println(thresholdExceeded ? "ON" : "OFF");
+  Serial.println();
+
+  delay(READ_INTERVAL_MS);
+}
